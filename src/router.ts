5a1f4b7774f53a5c1d2c1505fa2e299a2c28f153
Jun Huh <juan.huh@hotmail.com>
2018-12-07 21:10:20 +0100

    fixed login view

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,49 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import Profile from './views/Profile.vue';
+import Tracker from './views/Tracker.vue';
+import Dashboard from './views/Dashboard.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            component: Profile,
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: Dashboard,
+        },
+        {
+            path: '/tracker',
+            name: 'tracker',
+            component: Tracker,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+        },
+    ],
 });
---
+++ src/views/Tracker.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+  export default {
+    name: 'Tracker'
+  };
+</script>
+
+<style scoped>
+
+</style>
