7e607032949ec0b4109c4fc6e96eec14601976bb
Jun Huh <juan.huh@hotmail.com>
2018-12-10 22:03:02 +0100

    created registration

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,28 +4,33 @@
             <b-col md="4" offset-md="4">
                 <b-form @submit="onSubmit" @reset="onReset" v-if="show">
                     <b-form-group horizontal label-text-align="left" label="Email">
-                        <b-form-input id="exampleInput1"
-                                      type="email"
+                        <b-form-input type="email"
                                       v-model="form.email"
                                       required
                                       placeholder="Enter email">
                         </b-form-input>
                     </b-form-group>
                     <b-form-group horizontal label-text-align="left" label="Password">
-                        <b-form-input id="exampleInput2"
-                                      type="password"
+                        <b-form-input type="password"
                                       v-model="form.name"
                                       required
                                       placeholder="Enter password">
                         </b-form-input>
                     </b-form-group>
                     <br>
-                    <b-form-group id="exampleGroup4">
+                    <b-form-group>
                         <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
                             <b-form-checkbox value="me">Remember me</b-form-checkbox>
                         </b-form-checkbox-group>
                     </b-form-group>
-                    <b-button type="submit" variant="primary">Login</b-button>
+                    <b-button-toolbar id="button-group">
+                        <b-button-group class="mx-1">
+                            <b-button type="submit" variant="primary">Login</b-button>
+                        </b-button-group>
+                        <b-button-group class="mx-1">
+                            <b-button type="submit" variant="primary">Facebook</b-button>
+                        </b-button-group>
+                    </b-button-toolbar>
                 </b-form>
             </b-col>
         </b-form-row>
